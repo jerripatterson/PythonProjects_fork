98b47991476da344ab13fb169138bae9e4635eec
jerripat <jerripatterson@gmail.com>
2022-09-21 10:24:06 -0500

    Menu.py

--- menu.py
+++ menu.py
@@ -4,5 +4,18 @@ root = Tk()
 root.title("Hello World")
 root.geometry("400x400")
 
+my_label = Label(root, text="Hello World", fg="blue",
+                 bg="gold", font=("Helvetica", 20))
+my_label.pack()
+my_label2 = Label(root, text="More Hello World", relief="sunken")
+my_label2.pack()
+
+
+
+
+
+
+
+
 
 root.mainloop()
